dd88cb2952af127b48169f5bdd7c697a83aa4869
S-havl <pansillomcvalentincampostorres@gmail.com>
2026-05-15 11:04:06 -0500

    Nothign

--- src/boot/tool.py
+++
@@ -1,6 +0,0 @@
-def text_to_bin(input_file):
-    with open(input_file, 'r', encoding='utf-8') as f:
-        content = f.read().replace(' ', '').replace('\n', '').replace('r', '')
-        print(content)
-
-text_to_bin('stage1.bits')
